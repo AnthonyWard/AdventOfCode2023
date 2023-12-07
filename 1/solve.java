c4cb281b305f13d042c8b00f814d96a370487e05
Anthony Ward <pants1984@gmail.com>
2023-12-07 23:45:21 +0000

    improved java

--- 1/solve.java
+++ 1/solve.java
@@ -1,32 +1,31 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.*;
 import java.util.Scanner;
 import java.util.stream.*;
 
 public class Hello {
     public static void main(String[] args) {
         try {
-            File myObj = new File("C:\\git\\AdventOfCode2023\\1\\input.txt");
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String line = myReader.nextLine();
-                // convert to a char array and then find first number
-                char[] charArray = line.toCharArray();
-                int[] intArray = IntStream.range(0, charArray.length)
-                    .map(i -> Character.getNumericValue(charArray[i]))
-                    .toArray();
-                int first = intArray[0];
-                int last = intArray[intArray.length - 1];
-                // combine first and last to make a string
-                String firstString = Integer.toString(first);
-                String lastString = Integer.toString(last);
+            Path path = Paths.get("C:\\git\\AdventOfCode2023\\1\\input-small.txt");
+            var lines = Files.lines(path);
+            // iterate over lines and return a sum of the result
+            int sum = lines.map(line -> {
+                var firstNumber = line.chars().filter(x -> Character.isDigit((char) x)).findFirst().getAsInt();
+                var lastNumber = line.chars().filter(x -> Character.isDigit((char) x)).reduce((first, second) -> second).getAsInt();
+                // convert first and last number to string
+                String firstString = Character.toString(firstNumber);
+                String lastString = Character.toString(lastNumber);
+                // combine the strings
                 String combined = firstString + lastString;
                 // print
-                System.out.println(combined);
-
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
+                return Integer.parseInt(combined);
+            }).mapToInt(Integer::valueOf).sum();
+            
+            System.out.println(sum);
+            
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
