1d556759882b6c47f4e33c5c0035ee849b532eff
Anthony Ward <pants1984@gmail.com>
2023-12-08 00:19:24 +0000

    better java

--- 1/solve.java
+++ 1/solve.java
@@ -1,29 +1,24 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.*;
-import java.util.Scanner;
 import java.util.stream.*;
 
-public class Hello {
+public class Solve {
     public static void main(String[] args) {
         try {
-            Path path = Paths.get("C:\\git\\AdventOfCode2023\\1\\input-small.txt");
-            var lines = Files.lines(path);
-            // iterate over lines and return a sum of the result
-            int sum = lines.map(line -> {
-                var firstNumber = line.chars().filter(x -> Character.isDigit((char) x)).findFirst().getAsInt();
-                var lastNumber = line.chars().filter(x -> Character.isDigit((char) x)).reduce((first, second) -> second).getAsInt();
-                // convert first and last number to string
-                String firstString = Character.toString(firstNumber);
-                String lastString = Character.toString(lastNumber);
-                // combine the strings
-                String combined = firstString + lastString;
-                // print
-                return Integer.parseInt(combined);
-            }).mapToInt(Integer::valueOf).sum();
+            Path path = Paths.get("C:\\git\\AdventOfCode2023\\1\\input.txt");
+            var answer = Files
+                .lines(path)
+                .map(line ->
+                {
+                    var firstNumber = line.chars().filter(x -> Character.isDigit((char) x)).findFirst().getAsInt();
+                    var lastNumber = line.chars().filter(x -> Character.isDigit((char) x)).reduce((first, second) -> second).getAsInt();
+                    // combine numbers to make a two-digit number and convert to int so that it can be summed
+                    return Character.toString(firstNumber) + Character.toString(lastNumber);
+                })
+                .mapToInt(Integer::valueOf)
+                .sum();
             
-            System.out.println(sum);
+            System.out.println(answer);
             
         } catch (IOException e) {
             System.out.println("An error occurred.");
